fc25eafeb11e5c571a7268b311ca6bedbcb6a0cc
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2023-11-24 17:35:39 +0100

    created the picture table which has relation to the account table, implemented the create and update feature for uploading user images

--- server/controller/userInfo/handleUserInfo.js
+++ server/controller/userInfo/handleUserInfo.js
@@ -1,15 +1,69 @@
-const {PrismaClient} = require('@prisma/client');
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 
 
 
 
 
-exports.userProfilImage = (req,res) => {
 
+exports.userProfilImage = async(req,res) => {
+
+    const user = req.user 
+    console.log(user)
     const body =  req.file
 
     console.log(body)
 
-    res.json({message:'successful send'})
+
+    const pictureData = {
+        pictureUrl: req.file.filename,
+        picture_owner_id: req.user.userId
+    }
+
+    console.log(pictureData.pictureUrl)
+    console.log(pictureData.picture_owner_id)
+
+    try{
+        const exitPic = await prisma.picture.findUnique({
+            where: {picture_owner_id: req.user.userId}
+        })
+       
+
+        if(exitPic){
+           
+            const updatePic =  await prisma.picture.update({
+                where: {picture_owner_id: req.user.userId
+                }, 
+                data: {
+                    pictureUrl: pictureData.pictureUrl
+                },
+
+            })
+            console.log('updated picture')
+           
+            res.json({updatePic})
+        }else {
+
+            const createPic = await prisma.picture.create({
+                data: pictureData
+            })
+
+            console.log('new picture created')
+            res.json({createPic})
+        }
+    }
+
+    catch(error){
+        console.log(error)
+
+    } 
+    // finally {
+    //     await prisma.$disconnect();
+    // }
+
+
+    
+   
+
 
 }
