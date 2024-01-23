95d0e13f3040ae656fd5705a3501196a77a021a7
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-01-23 16:57:00 +0100

    adding  state handler and submit

--- components/exploreComponents/ExploreContent.jsx
+++ components/exploreComponents/ExploreContent.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '@styles/exploreStyle/explore.module.css'
 import GlobalHeader from '@utils/globalheader/GlobalHeader'
 
 const ExploreContent = () => {
+
+  
+  const [rangeValue, setrangeValue] = useState(15)
+  const [selectedValues, setSelectedValues] = useState({
+    selectTypeofEvent: "",
+    selectedRangeofEvents: "",
+    selectTypeofEventCategory: "",
+    startselectSearch: ""
+
+
+
+  })
+
+  const handleInput = (e) => {
+
+    if(e.target.className === "selectedRangeofEvents" && e.target.value)
+    setrangeValue(e.target.value)
+
+
+    setSelectedValues( selectedValues => {return {...selectedValues, [e.target.className]: e.target.value}})
+    
+  }
+
+
+
+
+
   return (
     <div>
       
        <section className={styles['middleSection']}>
             <div className={styles['middleContentarea']}>
+              
                 
                 <div className={styles['middleContentbar']}>
+
+                
+
+
                   <div className={styles['selectContentTypeofEventbar']}>
-                  <select className={styles['selectTypeofEvent']}  required>
+                  <select className={styles['selectTypeofEvent']}  onChange={handleInput} required>
                           <option value=''  className={styles['event-type-content']} >Event-type</option>
                           <option value="1">Exhibiton</option>
                           <option value={2}>House party</option>
@@ -19,17 +51,21 @@ const ExploreContent = () => {
                 </select>
                   </div>
                   <div>
-                <input type="range" name="" id="" />
+                  <input type="range" id="rangeInput"  className='selectedRangeofEvents'  onChange={handleInput} min="15" max="35" value={rangeValue} />
+                  <output for="rangeInput" id="outputValue">{rangeValue}</output>
 
                   </div>
                   <div className={styles['selectContentCategorybar']}>
-                  <select className={styles['selectTypeofEventCategory']}  required>
+                  <select className={styles['selectTypeofEventCategory']}  onChange={handleInput} required>
                           <option value=''  className={styles['event-Category-content']} >Event Category</option>
                           <option value="1">only friends</option>
                           <option value={2}>friends++</option>
                           <option value={3}>WorldWide</option>         
                 </select>
 
+                  </div>
+                  <div>
+                    <button className="startselectSearch">Start</button>
                   </div>
                
               
