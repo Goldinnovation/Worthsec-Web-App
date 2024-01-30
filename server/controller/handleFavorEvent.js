fd18377b79c8b49dcf3b0d24f9540afeec165244
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-01-30 23:50:16 +0100

    created database handler logic- create for join and favor feature

--- server/controller/handleFavorEvent.js
+++ server/controller/handleFavorEvent.js
@@ -1,12 +1,31 @@
 const {PrismaClient} = require('@prisma/client');
-const passport = require('passport')
-
+const prisma = new PrismaClient()
 
 
 exports.userFavorEvent = async(req,res) => {
 
-    const body = req.body
-    console.log(body)
-    res.json({message: "User connect to favor backend"})
+    const favor_user_id = req.user.userId
+    console.log(favor_user_id)
+    const favor_event_id = req.body.favoreventId
+    console.log(favor_event_id)
+
+    try{
+        if(req.user && req.body){
+
+            const userfavoredEvent =  await prisma.userFavorEvent.create({ data:
+            {
+                user_id:  favor_user_id,
+                event_id: favor_event_id
+            }})
+
+            console.log(userfavoredEvent)
+            res.status(200).json({message:"user successfully favored a event"})
+        }
+
+    }catch(error){
+        console.log('Error on server side:', error)
+        res.status(500).json({message: "Unexpected Error on serverside"})
+
+    }
 
 }
--- server/controller/handleJoinEvents.js
+++ server/controller/handleJoinEvents.js
@@ -1,12 +1,32 @@
 const {PrismaClient} = require('@prisma/client');
-const passport = require('passport')
+const prisma = new PrismaClient()
 
 
 
 exports.userJoinEvent = async(req,res) => {
 
-    const body = req.body
-    console.log(body)
-    res.json({message: "User connect to join backend"})
+    const joined_user_id = req.user.userId
+    console.log(joined_user_id)
+    const joined_event_id = req.body.joinEventId
+    console.log(joined_event_id)
+
+    try{
+        if(req.user && req.body){
+
+            const joinusertoEvent =  await prisma.userJoinEvent.create({ data:
+            {
+                user_id:  joined_user_id,
+                event_id: joined_event_id
+            }})
+
+            console.log(joinusertoEvent)
+            res.status(200).json({message:"user successfully favored a event"})
+        }
+
+    }catch(error){
+        console.log('Error on server side:', error)
+        res.status(500).json({message: "Unexpected Error on serverside"})
+
+    }
 
 }
