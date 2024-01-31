7ade980403c3cb1b090c831707c403a43728efa3
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-01-31 18:01:21 +0100

    created style of user feed and added new elements

---
+++ utils/userFeed/DisplayJoinEvent.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import styles from '@styles/joinandfavorstyle.module.css'
+
+
+
+
+export const allJointEventsbyUser = async() => {
+    try{
+        const res = await fetch('http://localhost:3000/api/DisplayJoinedEvent', {
+            method: "GET", 
+            cache: "force-cache"
+        })
+        if(!res.ok){
+            console.log('Unexpected Response Error');
+        }
+
+        const data = await res.json()
+        return data
+
+    }catch(error){
+        console.log('request from allJointEventsbyUser failed - Fetch Error:', error)
+    }
+}
+
+
+
+
+
+const DisplayJoinEvent = () => {
+    const [joineduserId, setJoinedUserId] = useState(null)
+
+useEffect(() => {
+        const fetchUserId = async() => {
+            const data = await allJointEventsbyUser();
+            setJoinedUserId(data)
+
+        }
+
+        // const intervalId = setInterval(fetchUserId, 5000)
+        fetchUserId(); 
+
+        // return () => clearInterval(intervalId)
+}, [])
+
+
+
+  return (
+    <>
+        <div className={styles['displayuserJoinedEvent']}>
+            sdasdas
+
+        </div>
+    </>
+  )
+}
+
+export default DisplayJoinEvent
