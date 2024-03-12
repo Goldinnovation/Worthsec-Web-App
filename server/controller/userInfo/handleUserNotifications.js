597a81bf2b4009ef4ae30345ceb9a76f0922ee6b
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-03-12 23:07:39 +0100

    connected the server to the backend for notificatiion, the handlerlogic checks the length of the notification for current user

--- server/controller/userInfo/handleUserNotifications.js
+++ server/controller/userInfo/handleUserNotifications.js
@@ -4,6 +4,24 @@ const prisma = new PrismaClient()
 
 
 
-exports.getUserNotification = (req,res) => {
-    res.json({message: "hjhuuiihiu"})
+exports.getUserNotification =  async (req,res) => {
+
+    const user = req.user
+   
+
+    try{
+        if(req.user){
+            const trackNotification = await prisma.notification.findMany({
+                where: {
+                    currentUser_Id: user.userId
+                }
+            })
+            console.log(trackNotification.length);
+            res.json(trackNotification)
+        }
+    }catch(error){
+        console.log(error)
+        return req.status(400).json({ message: "Error on getUserNotification hanlderlogic", error})
+
+    }
 }
--- utils/header/DisplayNotifications.jsx
+++ utils/header/DisplayNotifications.jsx
@@ -13,7 +13,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 const DisplayNotifications = () => {
 const [userNotification, setUserNotification] = useState(null)
 const {data: displayNotification, error} = useSWR('http://localhost:3000/api/notifications', fetcher,{
-   
+   refreshInterval: 5000
 })
 
 
