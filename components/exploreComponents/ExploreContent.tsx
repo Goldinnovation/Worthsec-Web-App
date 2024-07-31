6fa4f4ef3876bcb172acce831aaf45f34a5f6c0d
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-07-31 13:11:42 +0200

    updated the structure of exploreContent file, added a child component explore_feed

--- components/exploreComponents/ExploreContent.tsx
+++ components/exploreComponents/ExploreContent.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "@styles/exploreStyle/explore.module.css";
 import Image from "next/image";
+import ExploreFeed from "./Explore_feed";
 
 
 interface Event {
@@ -29,77 +30,26 @@ export const getallEventsWorldwide = async (selectedValues: string) => {
   }
 };
 
-//  User can favor event
-const userFavorEvent = async (favoreventId: string) => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/favorEvent`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ favoreventId }),
-    });
-    if (!res.ok) {
-      console.log("response Error on userFavorEvent");
-    }
 
-    const data = await res.json();
-  } catch (error) {
-    console.log("userFavorEvent request failed, fetch Error:", error);
-  }
-};
-
-// user can join event#
-
-const userJoinEvent = async (joinEventId: string) => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/JoinEvent`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ joinEventId }),
-    });
-    if (!res.ok) {
-      console.log("Response Error: UserJoinEvent fetch");
-    }
-    const data = await res.json();
-  } catch (error) {
-    console.log("Request Error: userJoinEvent Fetch, unexpected Error:", error);
-  }
-};
 
 const ExploreContent: React.FC = () => {
-  const [rangeValue, setrangeValue] = useState(9);
   const [userexploreData, setuserExploreData] = useState([]);
-  const [popupSelectedItem, setpopupSelectedItem] = useState(null);
-  const [favoreventId, setFavoreventId] = useState(null);
   const [selectedQuery, setSelectedQuery] = useState({
     explore_selectTypeofEvent__bmewZ: "3",
     selectedRangeofEvents: "",
     explore_selectTypeofEventCategory__KzDeU: "3",
   });
 
-  const handleInput = (e: any) => {
-    if (e.target.className === "selectedRangeofEvents" && e.target.value) {
-      setrangeValue(e.target.value);
-    }
 
-    setSelectedQuery({
-      ...selectedQuery,
-      [e.target.className]: e.target.value,
-    });
-  };
 
-  const handleToggleitem = (event: any) => {
-    setpopupSelectedItem(popupSelectedItem === event ? null : event);
-  };
+
 
   useEffect(() => {
     const fetchexploreData = async () => {
       try {
         const encodeData = encodeURI(JSON.stringify(selectedQuery));
         const exploreData = await getallEventsWorldwide(encodeData);
+       
         setuserExploreData(exploreData);
       } catch (error) {
         console.log("Error fetching explore data:", error);
@@ -115,60 +65,32 @@ const ExploreContent: React.FC = () => {
 
     // Renders the page from everytime the selectedValue is enterd
   }, [selectedQuery]);
+console.log(userexploreData);
 
   return (
     <div>
       <section className={styles["middleSection"]}>
         <div className={styles["middleContentarea"]}>
+          
+
           <div className={styles["middleContentbar_left"]}></div>
+
+
           <div className={styles["middleContentbar"]}>
-            <div className={styles["selectContentTypeofEventbar"]}>
-              <select
-                className={styles["selectTypeofEvent"]}
-                onChange={handleInput}
-                required
-              >
-                <option value="" className={styles["event-type-content"]}>
-                  Event-type
-                </option>
-                <option value="1">Exhibiton</option>
-                <option value={2}>House party</option>
-                <option value={3}>Club Party</option>
-              </select>
+            <div className={styles["EventStateArea"]}>
+              <div  className={styles["EventStateLable"]}>
+                Underground 
+              </div>
+             
             </div>
             <div>
-              <input
-                type="range"
-                id="rangeInput"
-                className="selectedRangeofEvents"
-                onChange={handleInput}
-                min="9"
-                max="20"
-                value={rangeValue}
-              />
-              <output htmlFor="rangeInput" id="outputValue">
-                {rangeValue}
-              </output>
             </div>
-            <div className={styles["selectContentCategorybar"]}>
-              <select
-                className={styles["selectTypeofEventCategory"]}
-                onChange={handleInput}
-                required
-              >
-                <option value="" className={styles["event-Category-content"]}>
-                  Event Category
-                </option>
-                <option value="1">only friends</option>
-                <option value={2}>friends++</option>
-                <option value={3}>WorldWide</option>
-              </select>
-            </div>
-            {/* <div>
-                    <button className="startselectSearch" onClick={() => getallEventsWorldwide(selectedValues)}>Start</button>
-                  </div> */}
+         
           </div>
           <div className={styles["middleContentbar_right"]}></div>
+
+
+
         </div>
         <div className={styles["exploreheader"]}></div>
         <div className={styles["middleContentFeed"]}>
@@ -181,177 +103,19 @@ const ExploreContent: React.FC = () => {
                 dsd
             </div>
           </div>
+
+
           <div className={styles["exploreEventContentRight"]}>
-            {/* <div className={styles['exploreEventSharedSection']} >
-                  <div className={styles['exploreEventSharedHead']}>
-                    dsdsd
-                  </div>
-                  <div>
-                      csdsd
-                  </div>
-                </div> */}
+           
             <div className={styles["explorefeedContent"]}>
               <div className={styles["explorefeedEventsHead"]}>
                 <div className={styles["explorefeedEventsHeadTitle"]}>
                  Category
                 </div>
               </div>
-              <div className={styles["explorefeedEvents"]}>
-                
-                {userexploreData.map((event: any, i) => (
-                  <div key={i} className={styles["exploreContentSection"]}>
-                    <div
-                      className={styles["exploreContent"]}
-                      onClick={() => handleToggleitem(event)}
-                    >
-                      <Image
-                        src={event?.ImageCoverUpload}
-                        width={200}
-                        alt="Event cover"
-                        height={200}
-                        quality={100}
-                      />
-                    </div>
-                    <div  className={styles["exploreEventTitleContent"]}>
-                      <div className={styles["exploreEventTitle"]}>
-                      {event.eventTitle}
-                      </div>
-                    </div>
-
-                    <div className={styles["exploreContentPopupSection"]}>
-                      {popupSelectedItem &&
-                        (popupSelectedItem as Event)?.eventId === event.eventId && (
-                          <div className={styles["exploreContentPopupOverlay"]}>
-                            <div className={styles["exploreContentPopupArea"]}>
-                              <div
-                                className={
-                                  styles["explorePopUpselectedTitleSection"]
-                                }
-                              >
-                                <h1
-                                  className={
-                                    styles["explorePopUpselectedTitle"]
-                                  }
-                                >
-                                  {event.eventTitle}
-                                </h1>
-                                <div
-                                  className={
-                                    styles["explorePopUpselectedClose"]
-                                  }
-                                  onClick={() => handleToggleitem(null)}
-                                ></div>
-                              </div>
-                              <div
-                                className={
-                                  styles["explorePopUpselectedContentSection"]
-                                }
-                              >
-                                <div
-                                  className={
-                                    styles["explorePopUpselectedContent"]
-                                  }
-                                >
-                                  <div
-                                    className={
-                                      styles["explorePopUpselectedCover"]
-                                    }
-                                  >
-                                    <Image
-                                      src={event.ImageCoverUpload}
-                                      alt="current user event cover"
-                                      className={
-                                        styles["explorePopUpCoverItem"]
-                                      }
-                                      width={650}
-                                      height={540}
-                                      quality={100}
-                                    />
-                                  </div>
-                                  <div
-                                    className={
-                                      styles["explorePopUpselectedDescript"]
-                                    }
-                                  >
-                                    {event.eventId}
-                                  </div>
-                                </div>
-                                <div
-                                  className={styles["explorePopUpselectedbar"]}
-                                >
-                                  dsndjn
-                                </div>
-                              </div>
-                              <div
-                                className={
-                                  styles["explorePopUpselectedOptions"]
-                                }
-                              >
-                                <button
-                                  className={
-                                    styles["explorePopUpselectedOptionbtn1"]
-                                  }
-                                  onClick={() => userJoinEvent(event.eventId)}
-                                >
-                                  Join
-                                </button>
-
-                                <button
-                                  className={
-                                    styles["explorePopUpselectedOptionbtn2"]
-                                  }
-                                  onClick={() => userFavorEvent(event.eventId)}
-                                >
-                                  Favor
-                                </button>
-
-                                <button
-                                  className={
-                                    styles["explorePopUpselectedOptionbtn3"]
-                                  }
-                                >
-                                  Share
-                                </button>
-                              </div>
-                            </div>
-
-                            <div className={styles["explorePopUppreview"]}>
-                              <div
-                                className={styles["explorePopUpmovebar"]}
-                              ></div>
-
-                              <div className={styles["explorePopUpOption"]}>
-                                {userexploreData.map((event: any, i) => (
-                                  <div
-                                    key={i}
-                                    className={
-                                      styles["explorePopuppreviewSection"]
-                                    }
-                                  >
-                                    <div
-                                      className={
-                                        styles["explorepreviewContent"]
-                                      }
-                                      onClick={() => handleToggleitem(event)}
-                                    >
-                                      <Image
-                                        src={event.ImageCoverUpload}
-                                        width={90}
-                                        height={90}
-                                        alt=" explore - event preview cover"
-                                        quality={100}
-                                      />
-                                    </div>
-                                  </div>
-                                ))}
-                              </div>
-                            </div>
-                          </div>
-                        )}
-                    </div>
-                  </div>
-                ))}
-              </div>
+              {/* Explore Feed */}
+                <ExploreFeed userexploreData={userexploreData} />
+           
             </div>
           </div>
         </div>
---
+++ components/exploreComponents/Explore_feed.tsx
@@ -0,0 +1,245 @@
+import styles from "@styles/exploreStyle/explore.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+
+
+interface userEvent{
+    ImageCoverUpload: string;
+    eventDate: string;
+    eventDescriptionContent: string;
+    eventHost: string;
+    eventId: string;
+    eventInviteType: number;
+    eventTime: string;
+    eventTitle: string;
+    eventType: number;
+
+}
+
+interface ExploreFeedprops{
+    userexploreData: userEvent[];
+}
+
+interface Event {
+    eventId: string;
+    
+  }
+
+
+
+
+
+//  User can favor event
+const userFavorEvent = async (favoreventId: string) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/favorEvent`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ favoreventId }),
+      });
+      if (!res.ok) {
+        console.log("response Error on userFavorEvent");
+      }
+  
+      const data = await res.json();
+    } catch (error) {
+      console.log("userFavorEvent request failed, fetch Error:", error);
+    }
+  };
+  
+  // user can join event#
+  
+  const userJoinEvent = async (joinEventId: string) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/JoinEvent`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ joinEventId }),
+      });
+      if (!res.ok) {
+        console.log("Response Error: UserJoinEvent fetch");
+      }
+      const data = await res.json();
+    } catch (error) {
+      console.log("Request Error: userJoinEvent Fetch, unexpected Error:", error);
+    }
+  };
+
+
+const ExploreFeed: React.FC<ExploreFeedprops> = ({userexploreData}) => {
+    const [popupSelectedItem, setpopupSelectedItem] = useState(null);
+
+
+    
+  const handleToggleitem = (event: any) => {
+    setpopupSelectedItem(popupSelectedItem === event ? null : event);
+  };
+
+
+
+    return(
+        <div className={styles["explorefeedEvents"]}>
+             {userexploreData.map((event: any, i) => (
+                  <div key={i} className={styles["exploreContentSection"]}>
+                    <div
+                      className={styles["exploreContent"]}
+                      onClick={() => handleToggleitem(event)}
+                    >
+                      <Image
+                        src={event?.ImageCoverUpload}
+                        width={200}
+                        alt="Event cover"
+                        height={200}
+                        quality={100}
+                      />
+                    </div>
+                    <div  className={styles["exploreEventTitleContent"]}>
+                      <div className={styles["exploreEventTitle"]}>
+                      {event.eventTitle}
+                      </div>
+                    </div>
+
+                    <div className={styles["exploreContentPopupSection"]}>
+                      {popupSelectedItem &&
+                        (popupSelectedItem as Event)?.eventId === event.eventId && (
+                          <div className={styles["exploreContentPopupOverlay"]}>
+                            <div className={styles["exploreContentPopupArea"]}>
+                              <div
+                                className={
+                                  styles["explorePopUpselectedTitleSection"]
+                                }
+                              >
+                                <h1
+                                  className={
+                                    styles["explorePopUpselectedTitle"]
+                                  }
+                                >
+                                  {event.eventTitle}
+                                </h1>
+                                <div
+                                  className={
+                                    styles["explorePopUpselectedClose"]
+                                  }
+                                  onClick={() => handleToggleitem(null)}
+                                ></div>
+                              </div>
+                              <div
+                                className={
+                                  styles["explorePopUpselectedContentSection"]
+                                }
+                              >
+                                <div
+                                  className={
+                                    styles["explorePopUpselectedContent"]
+                                  }
+                                >
+                                  <div
+                                    className={
+                                      styles["explorePopUpselectedCover"]
+                                    }
+                                  >
+                                    <Image
+                                      src={event.ImageCoverUpload}
+                                      alt="current user event cover"
+                                      className={
+                                        styles["explorePopUpCoverItem"]
+                                      }
+                                      width={650}
+                                      height={540}
+                                      quality={100}
+                                    />
+                                  </div>
+                                  <div
+                                    className={
+                                      styles["explorePopUpselectedDescript"]
+                                    }
+                                  >
+                                    {event.eventId}
+                                  </div>
+                                </div>
+                                <div
+                                  className={styles["explorePopUpselectedbar"]}
+                                >
+                                  dsndjn
+                                </div>
+                              </div>
+                              <div
+                                className={
+                                  styles["explorePopUpselectedOptions"]
+                                }
+                              >
+                                <button
+                                  className={
+                                    styles["explorePopUpselectedOptionbtn1"]
+                                  }
+                                  onClick={() => userJoinEvent(event.eventId)}
+                                >
+                                  Join
+                                </button>
+
+                                <button
+                                  className={
+                                    styles["explorePopUpselectedOptionbtn2"]
+                                  }
+                                  onClick={() => userFavorEvent(event.eventId)}
+                                >
+                                  Favor
+                                </button>
+
+                                <button
+                                  className={
+                                    styles["explorePopUpselectedOptionbtn3"]
+                                  }
+                                >
+                                  Share
+                                </button>
+                              </div>
+                            </div>
+
+                            <div className={styles["explorePopUppreview"]}>
+                              <div
+                                className={styles["explorePopUpmovebar"]}
+                              ></div>
+
+                              <div className={styles["explorePopUpOption"]}>
+                                {userexploreData.map((event: any, i) => (
+                                  <div
+                                    key={i}
+                                    className={
+                                      styles["explorePopuppreviewSection"]
+                                    }
+                                  >
+                                    <div
+                                      className={
+                                        styles["explorepreviewContent"]
+                                      }
+                                      onClick={() => handleToggleitem(event)}
+                                    >
+                                      <Image
+                                        src={event.ImageCoverUpload}
+                                        width={90}
+                                        height={90}
+                                        alt=" explore - event preview cover"
+                                        quality={100}
+                                      />
+                                    </div>
+                                  </div>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                        )}
+                    </div>
+                  </div>
+                ))}
+
+        </div>
+    )
+}
+
+export default ExploreFeed
