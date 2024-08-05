d7063a47e3459e6e0bc94b5da3fa82a8e8c1650b
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-08-05 19:01:54 +0200

    successfully created new record with new structure in db

--- server/controller/handleEvent.ts
+++ server/controller/handleEvent.ts
@@ -64,106 +64,122 @@ const createEvent = async (req: AuthenticatedRequest, res: Response) => {
     const trybody = req.body
     console.log(trybody)
 
-    // try {
-    //     if (!req.file) {
-    //         res.status(400).json({ message: "File could not be found" })
-    //     }
+    try {
+        if (!req.file) {
+            res.status(400).json({ message: "File could not be found" })
+        }
 
-    //     const file = req.file as Express.Multer.File;
+        const file = req.file as Express.Multer.File;
 
-    //     // storage path reference 
-    //     const dateTime = giveCurrentDateTime();
-    //     const storageRef = ref(storage, `files/${dateTime}_${file.originalname}`)
+        // storage path reference 
+        const dateTime = giveCurrentDateTime();
+        const storageRef = ref(storage, `files/${dateTime}_${file.originalname}`)
 
-    //     // compromising the image with sharp 
+        // compromising the image with sharp 
 
-    //     const compromiseImage = await sharp(file.buffer)
-    //         .resize({ width: 800, height: 1050 })
-    //         .jpeg({ quality: 80 })
-    //         .toBuffer();
-
-
-
-
-    //     const metadata = {
-    //         contentType: file.mimetype
-    //     }
-
-    //     // upload the file to the firebase storage
-    //     const uploadaction = uploadBytesResumable(storageRef, compromiseImage, metadata)
-
-    //     // wait for the upload to complete 
-    //     const snapshot = await uploadaction;
-
-    //     // gets the url of the post 
-    //     const downloadImageUrl = await getDownloadURL(snapshot.ref)
+        const compromiseImage = await sharp(file.buffer)
+            .resize({ width: 800, height: 1050 })
+            .jpeg({ quality: 80 })
+            .toBuffer();
 
 
 
-    //     // Converts the eventtype to an int
-    //     const stringEventType = req.body.eventType
-    //     const  IntEventType = parseInt(stringEventType, 10)
-    //     const userId = req.user.userId
-    //     console.log(downloadImageUrl);
-    //     console.log(req.body.Only_friends)
+
+        const metadata = {
+            contentType: file.mimetype
+        }
+
+        // upload the file to the firebase storage
+        const uploadaction = uploadBytesResumable(storageRef, compromiseImage, metadata)
+
+        // wait for the upload to complete 
+        const snapshot = await uploadaction;
+
+        // gets the url of the post 
+        const downloadImageUrl = await getDownloadURL(snapshot.ref)
 
 
-    //     let eventinviteNum;
-    //     if (req.body.Only_friends === '1') {
-    //         const num1 = req.body.Only_friends
-    //         eventinviteNum = parseInt(num1, 10)
-    //     } else if (req.body.friends_Plus_Plus === '2') {
-    //         const num2 = req.body.friends_Plus_Plus
-    //         eventinviteNum = parseInt(num2, 10)
-    //     } else if (req.body.worldwideClass === '3') {
-    //         const num3 = req.body.worldwideClass
-    //         eventinviteNum = parseInt(num3, 10)
-    //     }
+
+        //  declaration of object properties 
+        const userId = req.user.userId
+        const eventTitle_value = req.body.eventTitle
+        const eventDate_value = req.body.eventDate
+        const eventType_value = req.body.eventType
+        const zipcode_value = req.body.eventZipcode
+        console.log(zipcode_value);
+        const eventAddress_value = req.body.eventAddress
+        const cityType_value = req.body.eventType
+        const eventDescription_value = req.body.eventDescriptionContent
+        const eventTime_value = req.body.eventTime
+        let selectedRange_string = req.body.selectedRangeofEvents
+        const  selectedRange_int_value  = parseInt(selectedRange_string , 10)
+        console.log(selectedRange_int_value);
+        console.log(downloadImageUrl);
+        // console.log(req.body.Only_friends)
+
+
+        let eventinviteNum;
+        if (req.body.Only_friends === '1') {
+            const num1 = req.body.Only_friends
+            eventinviteNum = parseInt(num1, 10)
+        } else if (req.body.friends_Plus_Plus === '2') {
+            const num2 = req.body.friends_Plus_Plus
+            eventinviteNum = parseInt(num2, 10)
+        } else if (req.body.worldwideClass === '3') {
+            const num3 = req.body.worldwideClass
+            eventinviteNum = parseInt(num3, 10)
+        }
 
       
 
 
 
-    //     try {
+        try {
           
             
           
 
-    //         const newCreateEvent = await prisma.event.create({
-    //             data:
-    //             {
-    //                 eventHost: userId,
-    //                 eventTitle: req.body.eventTitle,
-    //                 eventType: IntEventType,
-    //                 eventDate: req.body.eventDate,
-    //                 eventDescriptionContent: req.body.eventDescriptionContent,
-    //                 eventTime: req.body.eventTime,
-    //                 ImageCoverUpload: downloadImageUrl,
-    //                 eventInviteType: eventinviteNum
+            const newCreateEvent = await prisma.event.create({
+                data:
+                {
+                    eventHost: userId,
+                    eventTitle: eventTitle_value,
+                    eventType: eventType_value,
+                    eventDate: eventDate_value ,
+                    eventDescriptionContent: eventDescription_value,
+                    eventTime: eventTime_value,
+                    ImageCoverUpload: downloadImageUrl,
+                    eventInviteType: eventinviteNum,
+                    eventAddress: eventAddress_value,
+                    eventZipcode: zipcode_value,
+                    cityType: cityType_value,
+                    selectedRangeofEvents: selectedRange_int_value
 
-    //             }
-    //         });
+
+                       
+
+                }
+            });
 
           
-    //         console.log(newCreateEvent, "successful uploaded")
-    //         res.status(200).json({ message: "file successful uploaded" });
+            console.log(newCreateEvent, "successful uploaded")
+            res.status(200).json({ message: "file successful uploaded" });
 
             
-    //         // res.status(200).json({messaage: "console.log('successful uploaded on the database');"})
+            // res.status(200).json({messaage: "console.log('successful uploaded on the database');"})
 
 
-    //     } catch (error) {
-    //         console.error(error)
-    //         res.status(500).send('issue server side')
-    //     }
+        } catch (error) {
+            console.error(error)
+            res.status(500).send('issue server side')
+        }
 
-    // } catch (error) {
+    } catch (error) {
 
-    //     console.log(error)
-    //     res.status(500).json({ message: "unexpected Error, trying to handle the file data" })
-    // }
+        console.log(error)
+        res.status(500).json({ message: "unexpected Error, trying to handle the file data" })
+    }
 
-    res.json({message:"create event is connected with backend "})
 
 
 }
