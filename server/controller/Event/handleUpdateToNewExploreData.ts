66942f418029711b2ba4192b5f2e04e25231b1fd
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-10-31 20:08:02 +0100

    Refactored the updatetoNewEventData function
    Removed unnecessary nesting by adding early return responses for invalid queries and replaced the ternary operator with an async function that includes a try-catch block to handle database query errors. Additionally, added single-line code comments and JSDoc comments.

--- server/controller/Event/handleUpdateToNewExploreData.ts
+++ server/controller/Event/handleUpdateToNewExploreData.ts
@@ -1,34 +1,29 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 import { Request, Response } from 'express';
-// const apicache = require('apicache');
-// const cache = apicache.middleware;
 
 
 
-interface eventProps {
-    eventId: string | null;
-    eventHost: string | null;
-     eventHostName: string | null;
-    eventTitle: string | null;
-    eventDate: Date;
-    eventType: string | null;
-    eventDescriptionContent: string | null;
-    eventTime: string | null;
-    ImageCoverUpload: string | null;
-    eventInviteType: number | null;
-    eventAddress: string | null;
-    eventZipcode: string | null;
-    cityType: string | null;
-    selectedRangeofEvents: number | null;
-  }
 
+
+/**
+ * Purpose Statement-- updatetoNewEventData
+ * The handler function receives an array of event IDs and checks for other IDs that do not match those.
+*/
+
+
+/**
+ * Function Signature--userFavourEvent
+ * @param {string} userId - The value represents the current user ID.
+ * @param {string} eventDataIds - The value represents the a list of event IDs.
+ * @returns {string} Returns a list of Events that matches the requirements 
+*/
 
 
 
 interface AuthenticatedRequest extends Request {
-    user?: any
-    decodedUserId: any
+  user?: any
+  decodedUserId: any
 
 }
 
@@ -38,35 +33,45 @@ interface AuthenticatedRequest extends Request {
 export async function updatetoNewEventData(req: Request, res: Response): Promise<void> {
     try {
         const userId = (req as AuthenticatedRequest)?.decodedUserId;
-        const eventDataArr = req.body?.EventDataId;
-        const eventDataLen = eventDataArr.length;
-        console.log(eventDataLen);
-        eventDataLen == 24 ?
-          (async () => {
-                const newEventData = await prisma.event.findMany({
-                  where: {
-                    eventId: {
-                      notIn: eventDataArr,
-                    },
-                  },
-                  take: 27,
-                });
-                // Adds new Data to the list
-                  res.status(200).json(newEventData)
-                console.log('isideCall', newEventData);
-            
-          })():
+        const eventDataIds = req.body?.EventDataId;
+      
+        // Checks if the user exist
+        if(!userId) {
+          res.status(400).json({ message: 'Invalid Request, userId does not exist' });
+        }
 
-           res.status(500).json({message: "Invalid Request on getNewEventData handler Function, Processing new event data "})
+        // Checks if the data exist and the length matches
+        if( !eventDataIds ||  eventDataIds.length !== 24 ) {
+          res.status(400).json({ message: 'User Length does not match the requirements' });
+        }
 
 
-    
+        // Function fetches new event data
+        const fetchNewEventData = async() => {
+          try{
+            const newEventData = await prisma.event.findMany({
+              where: {
+                eventId: {
+                  notIn: eventDataIds,
+                },
+              },
+              take: 27,
+            });
+         
+              res.status(200).json(newEventData)
 
+          }catch(error){
 
-     
+            // Handles Database Query Error
+            console.error("Error fetching new event data:", error);
+            res.status(500).json({ message: "Failed to fetch new event data", Error: error });
+          }}
+
+        await fetchNewEventData()
 
     } catch (error) {
         console.log("Server Error on getNewEventData handler function, CatchBlock - True:", error)
+        res.status(500).json({ message: "Unexpected error Internal Server Error" });
     }
 
 }
