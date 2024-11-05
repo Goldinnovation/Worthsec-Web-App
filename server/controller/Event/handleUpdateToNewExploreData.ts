528408030f7b1449e0fe69825eb6b7923ced4a72
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-11-05 14:14:22 +0100

    added a return statement on handleUpdatetoNewEvent function

--- server/controller/Event/handleUpdateToNewExploreData.ts
+++ server/controller/Event/handleUpdateToNewExploreData.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
+import prisma from '@/server/libs/prisma';
 import { Request, Response } from 'express';
 
 
@@ -8,14 +7,14 @@ import { Request, Response } from 'express';
 
 /**
  * Purpose Statement-- updatetoNewEventData
- * The handler function receives an array of event IDs and checks for other IDs that do not match those.
+ * The handler function receives an array of event IDs and checks for other event IDs that do not match those.
 */
 
 
 /**
  * Function Signature--userFavourEvent
  * @param {string} userId - The value represents the current user ID.
- * @param {string} eventDataIds - The value represents the a list of event IDs.
+ * @param {string} eventIds - The value represents the a list of event IDs.
  * @returns {string} Returns a list of Events that matches the requirements 
 */
 
@@ -31,47 +30,48 @@ interface AuthenticatedRequest extends Request {
 
 
 export async function updatetoNewEventData(req: Request, res: Response): Promise<void> {
-    try {
-        const userId = (req as AuthenticatedRequest)?.decodedUserId;
-        const eventDataIds = req.body?.EventDataId;
+  try {
+    const userId = (req as AuthenticatedRequest)?.decodedUserId;
+    const eventIds = req.body?.EventDataId;
 
-        if(!userId) {
-          res.status(400).json({ message: 'Invalid Request, userId does not exist' });
-        }
-
-   
-        if( !eventDataIds ||  eventDataIds.length !== 24 ) {
-          res.status(400).json({ message: 'User Length does not match the requirements' });
-        }
-
-
-        // Function fetches new event data
-        const fetchNewEventData = async() => {
-          try{
-            const newEventData = await prisma.event.findMany({
-              where: {
-                eventId: {
-                  notIn: eventDataIds,
-                },
-              },
-              take: 27,
-            });
-         
-              res.status(200).json(newEventData)
-
-          }catch(error){
-
-           
-            console.error("Error on fetching new event data, CatchBlock - True:", error);
-            res.status(500).json({ message: "Failed to fetch new event data", Error: error });
-          }}
-
-        await fetchNewEventData()
-
-    } catch (error) {
-        console.log("Server Error on updatetoNewEventData handler function, CatchBlock - True:", error)
-        res.status(500).json({ message: "Unexpected error Internal Server Error" });
+    if (!userId) {
+      res.status(400).json({ message: 'Invalid Request, userId does not exist' });
+      return
     }
+
+
+    if (!eventIds || eventIds.length !== 24) {
+      res.status(400).json({ message: 'Invalid Request: event Id length does not match the requirements' });
+      return
+    }
+
+    const fetchNewEventData = async () => {
+      try {
+        const newEventData = await prisma.event.findMany({
+          where: {
+            eventId: {
+              notIn: eventIds,
+            },
+          },
+          take: 27,
+        });
+
+        res.status(200).json(newEventData)
+
+      } catch (error) {
+
+
+        console.error("Error on fetching new event data, CatchBlock - True:", error);
+        res.status(500).json({ message: "Failed to fetch new event data", Error: error });
+      }
+    }
+
+    await fetchNewEventData()
+
+  } catch (error) {
+    console.log("Server Error on updatetoNewEventData handler function, CatchBlock - True:", error)
+    res.status(500).json({ message: "Unexpected error Internal Server Error", error });
+  }
 
 }
 
@@ -79,4 +79,4 @@ export async function updatetoNewEventData(req: Request, res: Response): Promise
 
 
 
-export default {  updatetoNewEventData }
+export default { updatetoNewEventData }
