ea0e2ed6c61c0975945af572cc742a4d8139a3ac
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2024-11-11 20:13:49 +0100

    refactored the handleUserInterestData function - added early return and exceptions

--- server/controller/User/handleuserInterestData.ts
+++ server/controller/User/handleuserInterestData.ts
@@ -1,9 +1,7 @@
 
 import { NextFunction, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient()
 import prisma from '../../libs/prisma';
-import mockprisma from "../../libs/prisma"
+
 
 
 
@@ -21,40 +19,41 @@ interface AuthenticatedRequest extends Request{
 
  const storeInterestData = async (req: Request, res: Response, next: NextFunction ) => {
 
+    try{
     const userId = (req as AuthenticatedRequest)?.decodedUserId
-    // const selectedInterests = (req as AuthenticatedRequest)?.userSelectedInterests
     const selectedInterests = req.body.pickedIntesrest
 
+    if (!userId || userId === undefined || userId === " ") {
+        res.status(400).json({ message: 'Invalid Request, userId is required' });
+        return;
+    }
 
+    if (!selectedInterests) {
+        res.status(400).json({ message: 'Invalid Request, selectedInterests is required' });
+        return;
+    }
 
-    try{
-        if(userId && selectedInterests){
-          
-            const storeUserSelectedInterestData = await prisma.userInterest.create({
+    
+    await prisma.userInterest.create({
                 data: {
                     user_interest_id: userId,
                     interest_list:  selectedInterests
                     
                 }
             })
-            // console.log("data:",storeUserSelectedInterestData);
-            res.status(200).json({message: "Interests are successfully stored"})
+            
+           
+    res.status(200).json({message: "Interests are successfully stored"})
 
-        }else{
-
-          
-
-            res.status(400).json({message: "Invalid Request Body data on the StoreInterestdata handler function"})
-        }
          
 
     }
     catch(error){
-        console.log('Errror on handler function storeInterestData', error);
-        res.status(400).json({message: "Catched Error on Response of handler storeInterstData"})
+        console.log("Server Error on storeInterestData handler function, CatchBlock - True:", error)
+        res.status(500).json({ message: "Internal Server Error" });
 
     }
-    // res.status(200).json({message:"connected"})
+   
 }
 
 export default storeInterestData
