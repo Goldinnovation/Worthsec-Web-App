db866bca5f1694f7f07ae0898722950a71422e01
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-04-29 12:07:14 +0200

    remove unecessary files

--- components/Header/headerComponent.jsx
+++ components/Header/headerComponent.jsx
@@ -1,7 +1,5 @@
 "use client"
 import React from "react"
-import LeftnavBarContent from "../Leftnavbar/navBarContent"
-import Settingsbtn from "@/utils/Settingsbtn"
 
 const HeaderComponent = () => {
  
@@ -20,7 +18,8 @@ const HeaderComponent = () => {
                 width: "100%",
                 height: "90%"
             }}>
-                <LeftnavBarContent/>
+                {/* <LeftnavBarContent/> */}
+                nav
             </div>
             <div style={{
                 // backgroundColor: "orange",
